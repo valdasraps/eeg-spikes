e0c6c6d72363b587624760f5cb4f887909592a35
Rapševičius Valdas <vrapsevicius@gmail.com>
2019-03-22 15:20:05 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'eeg_spikes',
+    version = '0.0.1',
+    url = 'https://github.com/valdasraps/eeg_spikes.git',
+    author = 'Valdas Rapsevicius',
+    author_email = 'valdas.rapsevicius@mif.vu.lt',
+    description = 'EEG spikes analysis tools',
+    packages = find_packages(),
+    install_requires = [],
+)
